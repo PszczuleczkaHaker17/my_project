82eeaf6e8fe509ac91038e234d706e72c9d290ee
Antoni Róg <Antekrog1@gmail.com>
2024-12-13 12:00:47 +0000

    skibidi7

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -9,15 +9,31 @@ const getDataFromNBP = async () => {
   const res = await fetch("https://api.nbp.pl/api/exchangerates/tables/A/?format=json")
   const jsonData = await res.json();
   rates.value = jsonData[0].rates
+  iloscWaluty.value = jsonData[0].rates.map(() => 0)
 }
 
-const kupWalute = async (index) => {
-  console.log(rates.value[index])
-  const ilosc = BigInt(iloscWaluty.value)
+const calculateCost = async (iloscWaluty, index) => {
+  const ilosc = BigInt(iloscWaluty)
   const cena = BigInt(rates.value[index].mid * 10e16)
 
   const koszt = await my_project_backend.calculate_currency_price(ilosc, cena)
-  console.log(koszt / BigInt(10e16))
+  return koszt / BigInt(10e16)
+}
+
+const kupWalute = async () => {
+  const koszta = await Promise.all(iloscWaluty.value.map((ilosc, index) => calculateCost(ilosc, index)))
+  const sum = koszta.reduce(
+    (al, val) => {
+      al + val
+    },
+    BigInt(0)
+  )
+}
+
+
+
+const onChange = (e, index) => {
+  iloscWaluty.value[index] = e.target.value
 }
 
 getDataFromNBP()
@@ -28,21 +44,21 @@ getDataFromNBP()
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
+    {{ iloscWaluty }}
     <table>
       <tr>
-        <th>Nazwa waluty</th>
-        <th>Kod waluty</th>
-        <th>Cena</th>
-        <th>Ilosc waluty do zakupu</th>
-        <th></th>
+        <th> Nazwa waluty </th>
+        <th> Kod waluty </th>
+        <th> Cena </th>
+        <th> Ilość waluty do zakupu</th>
       </tr>
       <tr v-for="(rate, index) in rates">
-        <td>{{ rate.currency }}</td>
-        <td>{{ rate.code }}</td>
-        <td>{{ rate.mid }}</td>
-        <td><input type="number" v-model="iloscWaluty"/></td>
-        <td><button @click="kupWalute(index)">Kup</button></td>
+        <td> {{ rate.currency }} </td>
+        <td> {{ rate.code }} </td>
+        <td> {{ rate.mid }} </td>
+        <td> <input type="number" @change="(e) => onChange(e, index)" @keydown="(e) => onChange(e, index)"></td>
       </tr>
     </table>
+    <button @click="kupWalute()"> Kup </button>
   </main>
 </template>
